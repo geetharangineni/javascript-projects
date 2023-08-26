e31daa2ab3cfcdf139f8e7e8e5b0642be05d59ae
geetharangineni <geetharangineni@gmail.com>
2023-08-26 14:16:30 -0500

    studio code for booleans and conditionals

--- how-to-write-code/consolelogexamples02.js
+++ how-to-write-code/consolelogexamples02.js
@@ -4,3 +4,34 @@ console.log("Python\nJavaScript\nJava\nC#\nSwift");
 console.log("python\tJavaScript\nJava\tC#\nSwift");
 
 
+//console.log(Boolean("false"));
+//console.log(Boolean("TRUE"));
+//console.log(Boolean(0));
+//console.log(Boolean(1));
+//console.log(Boolean(''));
+//console.log(Boolean('Launcode'));
+
+
+//console.log(0 == false)
+////console.log(3 + 4 === 7);
+//console.log("false");
+console.log(7 == "7");
+console.log(0 == false);
+console.log(0 == '');
+//console.log('0' == 0);
+//console.log(0 == '');
+//console.log(4 == "4");
+
+//console.log(4 < 3 || 2 < 3);
+
+
+
+let num = 7;
+
+if (num % 2 === 0) 
+{
+    console.log("Even");
+    if (num % 2 === 1) {
+        console.log("odd");
+    }
+}
