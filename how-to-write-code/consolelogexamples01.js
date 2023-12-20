78aa31d58a2f3b5247240549acd938dc7137a984
geetharangineni <geetharangineni@gmail.com>
2023-12-20 05:24:16 -0600

    For my practice

--- how-to-write-code/consolelogexamples01.js
+++ how-to-write-code/consolelogexamples01.js
@@ -4,3 +4,21 @@ console.log("What","do","commas","do?");
 console.log("Does", "adding",      "space", "matter?");
 console.log('Launch' + 'Code');
 console.log("LaunchCode was founded in", 2013);
+
+//const input = require('readline-sync');
+
+//let info = input.question("Question text... ");
+//const input = require('readline-sync');
+
+//const input = require('readline-sync');
+
+// info = input.question("Please enter your age: ");
+//The user enters 25.
+
+//console.log(typeof info);;
+const input = require('readline-sync');
+
+let num1 = input.question("Enter a number: ");
+let num2 = input.question("Enter another number: ");
+
+console.log(num1 + num2);
--- how-to-write-code/consolelogexamples02.js
+++ how-to-write-code/consolelogexamples02.js
@@ -1,5 +1,4 @@
 console.log("Some Programming Languages:");
-
 console.log("Python\nJavaScript\nJava\nC#\nSwift");
 console.log("python\tJavaScript\nJava\tC#\nSwift");
 
